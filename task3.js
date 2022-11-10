47928a8b7308a2050041999cad5266aad255aab2
Nikita Davydov <117898050+zerozafuru@users.noreply.github.com>
2022-11-10 20:05:27 +0300

    Create task3.js

---
+++ task3.js
@@ -0,0 +1,39 @@
+function solution(arr) {
+
+    let result = []
+    let i = 0
+    let last 
+    
+
+        while (i < arr.length-1) {
+            
+            if ((arr[i+1] - arr[i] === 1) && (arr[i+2] - arr[i+1] === 1)) {
+
+                let a = i
+
+                while (arr[a+1] - arr[a] === 1) {
+                    
+                    last = arr[a+1] 
+                    a++
+                    
+                }
+                
+                result.push(arr[i] + ' - ' + last)
+
+                i = a+1
+
+            } else {
+
+            result.push(arr[i])
+
+            i++
+
+            }
+
+        }
+
+        return(result)
+
+    }
+
+solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])    
