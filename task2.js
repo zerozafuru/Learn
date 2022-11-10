e548fddd968c3f103bfb3db1b675687f26cd9ec1
Nikita Davydov <117898050+zerozafuru@users.noreply.github.com>
2022-11-10 20:05:48 +0300

    Create task2.js

---
+++ task2.js
@@ -0,0 +1,23 @@
+let result = []
+
+function unbox(arg) {
+
+    arg.forEach((item) => {
+
+        if (Array.isArray(item)) {
+
+            unbox(item)
+
+        } else {
+
+            result.push(item)
+
+        }
+
+    });
+
+    return(result);
+
+}
+
+unbox([1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]])
