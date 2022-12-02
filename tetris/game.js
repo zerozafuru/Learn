b36cbd91a81ce631683ab15b1d8cb469b819befc
Nikita Davydov <117898050+zerozafuru@users.noreply.github.com>
2022-12-03 00:22:24 +0300

    Create game.js

---
+++ tetris/game.js
@@ -0,0 +1,94 @@
+export default class Game {
+    score = 0;
+    lines = 0;
+    level = 0;
+    playField = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+
+    activePiece = {
+        x: 0,
+        y: 0,
+        blocks: [
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 0, 0]
+        ]
+    };
+
+    movePieceLeft() {
+        this.activePiece.x -= 1;
+
+        if (this.hasCollision()) {
+            this.activePiece.x += 1;
+        }
+    };
+    movePieceRight() {
+        this.activePiece.x += 1;
+        if (this.hasCollision()) {
+            this.activePiece.x -= 1;
+        }
+    };
+    movePieceDown() {
+        this.activePiece.y += 1;
+        if (this.hasCollision()) {
+            this.activePiece.y += 1;
+            this.lockPiece();
+        }
+    };
+
+    hasCollision() {
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if (
+                    blocks[y][x] &&
+                    ((this.playField[pieceY + y] === undefined || this.playField[pieceY + y][pieceX + x] === undefined) ||
+                    this.playField[pieceY + y][pieceX + x])
+                ) {
+                    return true;
+                }
+                this.playField[pieceY + 0][pieceX + x] = blocks[y][x];
+
+            }
+
+        }
+
+        return false;
+    };
+
+    lockPiece() {
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+
+                if (blocks[y][x]) {
+
+                    this.playField[pieceY + 0][pieceX + x] = blocks[y][x];
+
+                }
+            }
+        }
+    }
+}
