c0dfab884dc9e452198a126f336614114d7d84aa
Nikita Davydov <117898050+zerozafuru@users.noreply.github.com>
2022-12-04 22:26:17 +0300

    Update game.js

--- tetris/game.js
+++ tetris/game.js
@@ -25,14 +25,58 @@ export default class Game {
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ];
 
+    piece = {
+        'I': [
+            [0, 0, 0, 0],
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ],
+        'J': [
+            [1, 0, 0],
+            [1, 1, 1],
+            [0, 0, 0]
+        ],
+        'L': [
+            [0, 0, 1],
+            [1, 1, 1],
+            [0, 0, 0]
+        ],
+        'O': [
+            [1, 1],
+            [1, 1]
+        ],
+        'S': [
+            [0, 1, 1],
+            [1, 1, 0],
+            [0, 0, 0]
+        ],
+        'Z': [
+            [1, 1, 0],
+            [0, 1, 1],
+            [0, 0, 0]
+        ],
+        'T': [
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 0, 0]
+        ]
+    };
+
+
+
     activePiece = {
         x: 0,
         y: 0,
+        get blocks() {
+            return this.rotations[this.rotationIndex];
+        },
         blocks: [
             [0, 1, 0],
             [1, 1, 1],
             [0, 0, 0]
         ]
+
     };
 
     movePieceLeft() {
@@ -56,6 +100,29 @@ export default class Game {
         }
     };
 
+    rotatePiece() {
+        const blocks = this.activePiece.blocks;
+        const length = blocks.length;
+
+        const temp = [];
+        for (let i = 0; i < length; i++) {
+            temp[i] = new Array(length).fill(0);
+
+        }
+        for (let y = 0; y < length; y++) {
+            for (let x = 0; x < length; x++) {
+                temp[x][y] = blocks[length - 1 - y][x];
+
+            }
+            if (this.hasCollision()) {
+                this.activePiece.blocks = blocks;
+
+            }
+        }
+
+        this.activePiece.blocks = temp;
+    }
+
     hasCollision() {
         const { y: pieceY, x: pieceX, blocks } = this.activePiece;
 
@@ -64,7 +131,7 @@ export default class Game {
                 if (
                     blocks[y][x] &&
                     ((this.playField[pieceY + y] === undefined || this.playField[pieceY + y][pieceX + x] === undefined) ||
-                    this.playField[pieceY + y][pieceX + x])
+                        this.playField[pieceY + y][pieceX + x])
                 ) {
                     return true;
                 }
