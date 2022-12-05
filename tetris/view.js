1a62f3143873bd4f662de408e66c40be7c506340
Nikita Davydov <117898050+zerozafuru@users.noreply.github.com>
2022-12-06 01:45:41 +0300

    Create view.js

---
+++ tetris/view.js
@@ -0,0 +1,48 @@
+export default class View {
+    constructor(element, width, height, rows, columns) {
+        this.element = element;
+        this.width = width;
+        this.height = height;
+
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+        this.context = this.canvas.getContext('2d');
+
+        this.blockWidth = this.idth / columns;
+        this.blockHeight = this.height / rows;
+
+        this.element.appendChild(this.canvas);
+
+    }
+    renderPlayField(playField) {
+        this.context.clearRect(0, 0, this.width, this.height);
+        for (let y = 0; y < playField.length; y++) {
+            const line = playField[y];
+            for (let x = 0; x < line.length; x++) {
+                const block = line[x];
+                if (block) {
+                    this.renderBlock(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight, 'red')
+                }
+
+            }
+
+        }
+
+    }
+    renderBlock(x, y, width, height, color) {
+        this.context.fillStyle = color;
+        this.context.strokeStyle = 'black';
+        this.context.lineWidth = 2;
+
+        this.context.fillRect(x, y, width, height);
+        this.context.strokeRect(x, y, width, height);
+    }
+    clearScreen() {
+        this.context.clearRect(0, 0, this.width, this.height);
+    }
+    render({ playField }) {
+        this.clearScreen();
+        this.renderPlayField(playField);
+    }
+}
