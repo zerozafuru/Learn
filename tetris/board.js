4e18991ecc5c515b96f3a29e00eb57ced3672194
Nikita Davydov <officialnikitadavydov@gmail.com>
2022-12-06 17:43:52 +0300

    working

---
+++ tetris/board.js
@@ -0,0 +1,175 @@
+class Board {
+  constructor(ctx, ctxNext) {
+    this.ctx = ctx;
+    this.ctxNext = ctxNext;
+    this.piece = null;
+    this.next = null;
+  }
+
+  reset() {
+    this.grid = this.getEmptyBoard();
+    this.piece = new Piece(this.ctx);
+    this.piece.setStartPosition();
+    this.getNewPiece();
+  }
+
+  getNewPiece() {
+    this.next = new Piece(this.ctxNext);
+    this.ctxNext.clearRect(
+      0,
+      0,
+      this.ctxNext.canvas.width,
+      this.ctxNext.canvas.height
+    );
+    this.next.draw();
+  }
+
+  getEmptyBoard() {
+    return Array.from(
+      { length: ROWS }, () => Array(COLS).fill(0)
+    );
+  }
+  insideWalls(x) {
+    return x >= 0 && x < COLS;
+  }
+
+  aboveFloor(y) {
+    return y <= ROWS;
+  }
+
+  notOccupied(x, y) {
+    return this.grid[y] && this.grid[y][x] === 0;
+  }
+
+  valid(p) {
+    return p.shape.every((row, dy) => {
+      return row.every((value, dx) => {
+        let x = p.x + dx;
+        let y = p.y + dy;
+        return value === 0 ||
+          (this.insideWalls(x) && this.aboveFloor(y) && this.notOccupied(x, y));
+      });
+    });
+
+  }
+
+  rotate(p) {
+
+      for (let y = 0; y < p.shape.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+          [p.shape[x][y], p.shape[y][x]] =
+            [p.shape[y][x], p.shape[x][y]];
+        }
+      }
+
+      p.shape.forEach(row => row.reverse());
+
+      let clone = JSON.parse(JSON.stringify(p));
+      return clone;
+    
+  }
+
+  draw() {
+    this.piece.draw();
+    this.drawBoard();
+  }
+
+  drawBoard() {
+    this.grid.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value > 0) {
+          this.ctx.fillStyle = COLORS[value];
+          this.ctx.fillRect(x, y, 1, 1);
+        }
+      });
+    })
+  }
+
+  freeze() {
+    this.piece.shape.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value > 0) {
+          this.grid[y + this.piece.y][x + this.piece.x] = value;
+        }
+      });
+    });
+  }
+
+  drop() {
+    let p = moves[KEY.DOWN](this.piece);
+    if (this.valid(p)) {
+      this.piece.move(p);
+    } else {
+      this.freeze();
+      this.clearLines();
+      console.table(this.grid);
+
+      if (this.piece.y === 0) {
+
+        return false;
+      }
+
+      this.piece = this.next;
+      this.piece.ctx = this.ctx;
+      this.piece.setStartPosition();
+      this.getNewPiece();
+    }
+
+    return true;
+  }
+
+  getLineClearPoints(lines) {
+    return lines === 1 ? POINTS.SINGLE :
+      lines === 2 ? POINTS.DOUBLE :
+        lines === 3 ? POINTS.TRIPLE :
+          lines === 4 ? POINTS.TETRIS :
+            0;
+  }
+
+  getLinesClearPoints(lines, level) {
+    const lineClearPoints =
+      lines === 1
+        ? POINTS.SINGLE
+        : lines === 2
+          ? POINTS.DOUBLE
+          : lines === 3
+            ? POINTS.TRIPLE
+            : lines === 4
+              ? POINTS.TETRIS
+              : 0;
+
+    return (level + 1) * lineClearPoints;
+  }
+
+  clearLines() {
+    let lines = 0;
+
+    this.grid.forEach((row, y) => {
+
+      if (row.every(value => value > 0)) {
+        lines++;
+
+        this.grid.splice(y, 1);
+
+        this.grid.unshift(Array(COLS).fill(0));
+      }
+    });
+
+    if (lines > 0) {
+
+      account.score += this.getLinesClearPoints(lines, account.level);
+      account.lines += lines;
+
+      if (account.lines >= LINES_PER_LEVEL) {
+
+        account.level++;
+
+        account.lines -= LINES_PER_LEVEL;
+
+        time.level = LEVEL[account.level];
+
+      }
+    }
+
+  }
+}
---
+++ tetris/constants.js
@@ -0,0 +1,81 @@
+const COLS = 10;
+const ROWS = 20;
+const BLOCK_SIZE = 30;
+
+const KEY = {
+  UP: 38,
+  SPACE: 32,
+  LEFT: 37,
+  RIGHT: 39,
+  DOWN: 40
+}
+
+Object.freeze(KEY);
+
+const POINTS = {
+  SINGLE: 100,
+  DOUBLE: 300,
+  TRIPLE: 500,
+  TETRIS: 800,
+  SOFT_DROP: 1,
+  HARD_DROP: 2
+}
+
+Object.freeze(POINTS);
+
+const LINES_PER_LEVEL = 10;
+
+const LEVEL = {
+  0: 800,
+  1: 720,
+  2: 630,
+  3: 550,
+  
+}
+
+Object.freeze(LEVEL);
+
+const COLORS = [
+  'red',
+  'cyan',
+  'blue',
+  'orange',
+  'yellow',
+  'green',
+  'purple'
+  
+];
+
+const SHAPES =  [
+
+  [[0, 0, 0, 0],
+   [1, 1, 1, 1],  
+   [0, 0, 0, 0],
+   [0, 0, 0, 0]],
+
+  [[2, 0, 0], 
+   [2, 2, 2], 
+   [0, 0, 0]],
+
+  [[0, 0, 3], 
+   [3, 3, 3], 
+   [0, 0, 0]],
+
+  [[4, 4], 
+   [4, 4]],
+
+  [[0, 5, 5], 
+   [5, 5, 0], 
+   [0, 0, 0]],
+
+  [[0, 6, 0], 
+   [6, 6, 6], 
+   [0, 0, 0]],
+
+  [[7, 7, 0], 
+   [0, 7, 7], 
+   [0, 0, 0]]
+
+];
+
+
